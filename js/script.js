e1c66135f4a71463637a3ad73af3d4fa6fbdd1bd
Asrafali9 <asrafaliofficial009@gmail.com>
2021-07-05 15:11:15 +0530

    first push

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
   })
 });
+
+let theme_icon=document.querySelector(".btn4");
+let logo=document.querySelector(".logo");
+let abt_meal_img=document.querySelector(".abt_meal_img");
+let root_change=document.querySelector(".root_change");
+
+console.log(root_change);
+console.log(theme_icon);
+console.log(logo);
+console.log(abt_meal_img);
+theme_icon.addEventListener('click',()=>{
+  console.log("object");
+  document.body.classList.toggle('root_change');
+  if(document.body.classList.contains("root_change")){
+    logo.src="images/logodark.png";
+    abt_meal_img.src="images/about_meal.jpg"
+  }else{  
+    logo.src="icons/logo.svg";
+    abt_meal_img.src="images/about_meal.png"
+
+  }
+
+})
